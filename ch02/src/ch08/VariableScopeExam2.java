144152006138d40a0918ac344811d1fd04780233
user <user@DESKTOP-TE3I442>
2021-09-17 15:02:35 +0900

    123

---
+++ ch02/src/ch08/VariableScopeExam2.java
@@ -0,0 +1,14 @@
+package ch08;
+
+public class VariableScopeExam2 {
+	public static void main(String[] args) {
+		
+		//main메소드내의 if블럭에서 선언된 로컬변수
+		int var2;
+		int var3;
+		var1 = 1; //값 변경
+		
+		
+		
+	}
+}
