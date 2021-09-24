eac73feacfcf0b683d060f80445054b0d713f186
zlaxo <zlaxo@112.168.96.49>
2021-09-24 16:24:12 +0900

    ㅁㄴ

---
+++ ch04/src/ch02_switch/SwitchExample3.java
@@ -0,0 +1,25 @@
+package ch02_switch;
+
+import java.util.Scanner;
+
+public class SwitchExample3 {
+
+	public static void main(String[] args) {
+		
+		//월요일, 화요일, 수요일, 목요일, 금요일, 토요일, 일요일
+		//월~목요일 : 열심히 공부합시다.
+		//금요일 : 쇼핑을 합니다.
+		//토요일, 일요일 : 레저를 즐깁니다.
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("요일을 입력해주세요(ex:월요일): ");
+		String weekDay = scanner.next();
+		
+		switch(weekDay) {
+		case "월요일" : case "화요일": case "수요일" : case "목요일" : System.out.println("열심히 공부합시다");break;
+		case "금요일" : System.out.println("쇼핑을 합니다.");break;
+		case "토요일" : case "일요일" : System.out.println("레저를 즐깁니다.");break;
+		default:System.out.println("다시 입력해주세요.");
+		}
+	}
+
+}
