e06f085c05f059c60f31343fc0faea3912153941
zlaxo <zlaxo@112.168.96.49>
2021-09-24 17:21:18 +0900

    v

---
+++ ch04/src/ch03_for/ForExam01.java
@@ -0,0 +1,12 @@
+package ch03_for;
+
+public class ForExam01 {
+	public static void main(String[] args) {
+		
+		//명령문의 갯수 = 4
+		for(int i=1;i<=10; i++) {
+			System.out.println(i);
+			
+		}
+	}
+}
